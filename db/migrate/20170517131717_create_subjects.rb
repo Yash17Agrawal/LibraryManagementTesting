92fd4e323d6fe0bdde54d9c6b478cc423f6afb35
YASH\code_eagle <yash17agrawal@gmail.com>
2017-06-03 17:10:07 +0530

    Added Controller functions

--- db/migrate/20170517131717_create_subjects.rb
+++ db/migrate/20170517131717_create_subjects.rb
@@ -1,8 +1,15 @@
 class CreateSubjects < ActiveRecord::Migration[5.1]
-  def change
+  def self.up
     create_table :subjects do |t|
-
-      t.timestamps
+    	t.column :name, :string
     end
+    Subject.create :name => "Physics"
+    Subject.create :name => "Maths"
+    Subject.create :name => "Chemistry"
+    Subject.create :name => "Psychology"
+    Subject.create :name => "Geography"
+  end
+  def self.down
+  	drop_table :subjects
   end
 end
